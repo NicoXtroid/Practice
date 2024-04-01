d023289371d0ccaab54c6c2619123c2447abc2d4
NicoXtroid <nicole.riova@gmail.com>
2024-04-01 00:34:56 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/demo/geolocation/ui/components/MenuButton.kt
@@ -0,0 +1,23 @@
+package com.example.demo.geolocation.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MenuButton(label: String, onClick: ()-> Unit = {}){
+    Button(modifier = Modifier.padding(22.dp), onClick = { /*TODO*/ }) {
+        Text(
+            text = label,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 5.dp),
+            textAlign = TextAlign.Center
+        )
+    }
+}
